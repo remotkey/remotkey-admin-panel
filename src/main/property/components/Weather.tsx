b5dbe74fa7c3f5e414768b2bf4cf7024b35c5f4b
piyushpathar <piyu.pathar@gmail.com>
2024-10-11 21:32:40 +0530

    test

--- src/main/property/components/Weather.tsx
+++ src/main/property/components/Weather.tsx
@@ -24,7 +24,6 @@ export const Weather = async ({ placeName }: { placeName: string }) => {
           alt="Weather"
         />
       )}
-
       <span className="mb-2 text-4xl font-bold text-C_002E2E">
         {(weather?.main?.temp).toFixed(2)}°F
       </span>
